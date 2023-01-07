12b10a37e5487728102509e6fc91324a37a1537c
Dario Mintzer <dario.mintzer.ext@kavak.com>
2023-01-07 12:36:28 -0300

    Started desktop version

--- shoes_app/lib/src/pages/shoe_page_desktop.dart
+++ shoes_app/lib/src/pages/shoe_page_desktop.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+
+import '../widgets/custom_widgets.dart';
 
 class ShoePageDesktop extends StatelessWidget {
   const ShoePageDesktop({super.key});
@@ -6,8 +10,47 @@ class ShoePageDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.amber,
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 300,
+                // color: Colors.grey,
+                child: CustomAppBar(text: 'For you'),
+              ),
+              Container(
+                width: 300,
+                child: AddToCart(text: 'Add to Cart', amount: 180),
+              )
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Hero(
+                    tag: 'shoe-1',
+                    child: ShoeCard(),
+                  ),
+                  ShoeDesc(
+                    title: 'Nike Air Max 720',
+                    description:
+                        'In Amet commodo anim cupidatat irure ea dolore irure anim. Ea adipisicing nisi reprehenderit do irure ut non occaecat eu cillum sunt Lorem pariatur eiusmod. Est Lorem ipsum elit id in dolor fugiat.',
+                  ),
+                  ShoeCard(),
+                  ShoeDesc(
+                    title: 'Nike Air Max 720',
+                    description:
+                        'In Amet commodo anim cupidatat irure ea dolore irure anim. Ea adipisicing nisi reprehenderit do irure ut non occaecat eu cillum sunt Lorem pariatur eiusmod. Est Lorem ipsum elit id in dolor fugiat.',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
